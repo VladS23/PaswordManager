b1a2393fbd20484345a9236a22e8de87c2487f5a
VladS23 <77384665+VladS23@users.noreply.github.com>
2023-03-08 23:37:12 +0300

    upd password window

--- PaswordManager/Passwords.xaml.cs
+++ PaswordManager/Passwords.xaml.cs
@@ -25,14 +25,34 @@ namespace PaswordManager
             ListBox passwordsListBox = new();
             List<Grid> rows = new();
             Grid firstRow = new();
-            RowDefinition site = new();
-            RowDefinition login = new();
-            RowDefinition password = new();
-            RowDefinition comment = new();
-            firstRow.RowDefinitions.Add(site);
-            firstRow.RowDefinitions.Add(login);
-            firstRow.RowDefinitions.Add(password);
-            firstRow.RowDefinitions.Add(comment);
+            ColumnDefinition site = new();
+            ColumnDefinition login = new();
+            ColumnDefinition password = new();
+            ColumnDefinition comment = new();
+            firstRow.ColumnDefinitions.Add(site);
+            firstRow.ColumnDefinitions.Add(login);
+            firstRow.ColumnDefinitions.Add(password);
+            firstRow.ColumnDefinitions.Add(comment);
+            TextBlock siteTextBlock = new();
+            siteTextBlock.Text = "Сайт";
+            TextBlock loginTextBlock = new();
+            loginTextBlock.Text = "Логин";
+            TextBlock passwordTextBlock = new();
+            passwordTextBlock.Text = "Пароль";
+            TextBlock commentTextBlock = new();
+            commentTextBlock.Text = "Комментарий";
+            Grid.SetColumn(siteTextBlock, 0);
+            Grid.SetColumn(loginTextBlock, 1);
+            Grid.SetColumn(passwordTextBlock, 2);
+            Grid.SetColumn(commentTextBlock, 3);
+            firstRow.Children.Add(siteTextBlock);
+            firstRow.Children.Add(loginTextBlock);
+            firstRow.Children.Add(passwordTextBlock);
+            firstRow.Children.Add(commentTextBlock);
+            firstRow.Width = this.Width;
+            passwordsListBox.Items.Add(firstRow);
+            Grid.SetRow(passwordsListBox, 0);
+            mainGrid.Children.Add(passwordsListBox);
 
         }
     }
