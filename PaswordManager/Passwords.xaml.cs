06aef6c732b227d96ef6b34f8424b9a50559f1b1
VladS23 <77384665+VladS23@users.noreply.github.com>
2023-04-03 11:47:27 +0300

    add create

--- PaswordManager/Passwords.xaml.cs
+++ PaswordManager/Passwords.xaml.cs
@@ -19,41 +19,57 @@ namespace PaswordManager
     /// </summary>
     public partial class Passwords : Window
     {
+        public List<Grid> rows = new();
+        public ListBox passwordsListBox = new();
         public Passwords()
         {
             InitializeComponent();
-            ListBox passwordsListBox = new();
-            List<Grid> rows = new();
-            Grid firstRow = new();
-            ColumnDefinition site = new();
-            ColumnDefinition login = new();
-            ColumnDefinition password = new();
-            ColumnDefinition comment = new();
-            firstRow.ColumnDefinitions.Add(site);
-            firstRow.ColumnDefinitions.Add(login);
-            firstRow.ColumnDefinitions.Add(password);
-            firstRow.ColumnDefinitions.Add(comment);
+            addPasswordRecord("Сайт", "Логин", "Пароль", "Комментарий");
+            AddButton.Click += CreateButtonClicked;
+        }
+
+        private void CreateButtonClicked(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addPasswordRecord(string site, string log, string pasw, string comm)
+        {
+            Grid r = new();
+            ColumnDefinition siteColumn = new();
+            ColumnDefinition loginColumn = new();
+            ColumnDefinition passwordColumn = new();
+            ColumnDefinition commentColumn = new();
+            r.ColumnDefinitions.Add(siteColumn);
+            r.ColumnDefinitions.Add(loginColumn);
+            r.ColumnDefinitions.Add(passwordColumn);
+            r.ColumnDefinitions.Add(commentColumn);
             TextBlock siteTextBlock = new();
-            siteTextBlock.Text = "Сайт";
+            siteTextBlock.Text = site;
             TextBlock loginTextBlock = new();
-            loginTextBlock.Text = "Логин";
+            loginTextBlock.Text = log;
             TextBlock passwordTextBlock = new();
-            passwordTextBlock.Text = "Пароль";
+            passwordTextBlock.Text = pasw;
             TextBlock commentTextBlock = new();
-            commentTextBlock.Text = "Комментарий";
+            commentTextBlock.Text = comm;
             Grid.SetColumn(siteTextBlock, 0);
             Grid.SetColumn(loginTextBlock, 1);
             Grid.SetColumn(passwordTextBlock, 2);
             Grid.SetColumn(commentTextBlock, 3);
-            firstRow.Children.Add(siteTextBlock);
-            firstRow.Children.Add(loginTextBlock);
-            firstRow.Children.Add(passwordTextBlock);
-            firstRow.Children.Add(commentTextBlock);
-            firstRow.Width = this.Width-29;
-            passwordsListBox.Items.Add(firstRow);
+            r.Children.Add(siteTextBlock);
+            r.Children.Add(loginTextBlock);
+            r.Children.Add(passwordTextBlock);
+            r.Children.Add(commentTextBlock);
+            r.Width = this.Width - 29;
+            passwordsListBox.Items.Add(r);
             Grid.SetRow(passwordsListBox, 0);
+            UpdateMainGrid(passwordsListBox);
+            rows.Add(r);
+        }
+        public void UpdateMainGrid(ListBox passwordsListBox)
+        {
+            mainGrid.Children.Clear();
             mainGrid.Children.Add(passwordsListBox);
-
         }
     }
 }
